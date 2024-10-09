10c29e77c38076aed381e79d702f09c91c1a6e0a
barbare110 <barbarebarbare024@gmail.com>
2024-10-09 16:44:54 +0400

    second commit

---
+++ day.04/homework4(5).py
@@ -0,0 +1,12 @@
+# Define variables of different data types
+variable1 = 50             
+variable2 = 3.14            
+variable3 = "Hello,World!"  
+variable4 = True             
+
+# Print the type of each variable
+
+print(type(variable1))  
+print(type(variable2))  
+print(type(variable3))  
+print(type(variable4)) 
