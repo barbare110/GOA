beba75e23a43a102a6b08980b55be6cc13b82b9c
barbare110 <barbarebarbare024@gmail.com>
2025-03-13 23:50:41 +0400

    commit

---
+++ day.21/classwork/cw.py
@@ -0,0 +1,14 @@
+# carbrands = ["Ferrari,  Bently, mercedes, BMW, Sudi,"]
+# print(carbrands)
+
+# # 2)
+# food = ["random foods"   ]
+# print(food)
+
+# # 3)
+# num=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+# print(num [1;9])
+
+# num=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+
+# print( num[4;1]) 
